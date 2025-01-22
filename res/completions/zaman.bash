0e1073fa74e2884c24f0ea4019fd131fb6f10a40
Robin Candau <robincandau@protonmail.com>
2025-01-22 21:06:32 +0100

    feat: Add dynamic shell completions for man pages
    Add dynamic shell completions for man pages for bash, zsh & fish; so man pages can now be autocompleted with `zaman [TAB]` (the same way it would be done with plain `man`).

--- res/completions/zaman.bash
+++ res/completions/zaman.bash
@@ -1,11 +1,16 @@
 _zaman() {
 	local arg="${2}"
 	local -a opts 
+
 	opts=('-m --menu
 	       -o --output
 	       -O --save
+	       -D --debug
 	       -h --help
 	       -V --version')
+
+	local man_pages=$(man -k . | awk '{print $1}')
+	opts+=("${man_pages}")
 
 	COMPREPLY=( $(compgen -W "${opts[*]}" -- "${arg}") )
 }
--- res/completions/zaman.zsh
+++ res/completions/zaman.zsh
@@ -5,8 +5,11 @@ opts=(
     {-m,--menu}'[Print a menu via rofi or dmenu that lists every man pages to choose from (default operation)]'
     {-o,--output}'[(Args <man page> <file>) Save <man page> into the <file> PDF file]'
     {-O,--save}'[(Arg <man page>) Save <man page> into the "man_<man page>.pdf" file in the current directory]'
+    {-D,--debug}'[Display debug traces]'
     {-h,--help}'[Display the help message]'
     {-V,--version}'[Display version information]'
 )
 
-_arguments $opts
+local man_pages=(${(f)"$(man -k . | awk '{print $1}')"})
+
+_arguments $opts '*::man-pages:(${man_pages})'
