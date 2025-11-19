5d2a82c9ae3b18ca7d5555429b5a97968381e19d
Robin Candau <robincandau@protonmail.com>
2025-11-19 22:44:52 +0100

    chore(release): Improve the release process
    Add a dedicated script to cut new releases

---
+++ scripts/mkrelease.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+set -euo pipefail
+
+# Check that we are on the main branch
+if [ "$(git branch --show-current)" != "main" ]; then
+	echo -e >&2 "ERROR: Current branch is not 'main'"
+	exit 1
+fi
+
+# Check that `github-cli` is correctly authenticated
+if ! gh auth status > /dev/null; then
+	echo -e >&2 "ERROR: github-cli is not authenticated"
+	exit 2
+fi
+
+# Pull repo and fetch tags
+git pull
+git fetch --tags
+
+# Assign and check tag variables
+release_tag="${1}"
+latest_tag=$(git describe --tags --abbrev=0)
+
+if [ -z "${release_tag}" ]; then
+	echo -e >&2 "ERROR: Release tag is empty\nUsage: ./scripts/mkrelease.sh X.Y.Z # where 'X.Y.Z' is the tag to create"
+	exit 3
+fi
+
+echo -e "Release tag = v${release_tag}\nLatest tag = ${latest_tag}\n"
+read -rp "Confirm? [y/N] " answer
+
+case "${answer}" in
+	y|Y)
+		echo -e "\nProceeding in 5 sec"
+		sleep 5
+	;;
+	*)
+		echo -e >&2 "\nAborting"
+		exit 4
+	;;
+esac
+
+# Bump version where necessary
+sed -i "s/$(zaman -V | cut -f2 -d " ")/${release_tag}/g" doc/man/zaman.* src/zaman.sh
+
+# Update changelog
+git-cliff -up CHANGELOG.md
+sed -i "s|\[unreleased\]|\[v${release_tag}\](https://github.com/Antiz96/zaman/releases/tag/v${release_tag})\ -\ $(date '+%Y-%m-%d')|g" CHANGELOG.md
+
+# Review changes
+git diff
+echo
+read -rp "Confirm? [y/N] " answer
+
+case "${answer}" in
+	y|Y)
+		echo -e "\nProceeding in 5 sec"
+		sleep 5
+	;;
+	*)
+		echo -e >&2 "\nAborting"
+		exit 5
+	;;
+esac
+
+# Create and push a signed commit
+git add .
+git commit -SD33FAA16B937F3B2 -m "chore(release): v${release_tag}"
+git push
+
+# Create and push a signed tag
+git tag "v${release_tag}" -u D33FAA16B937F3B2 -m "v${release_tag}"
+git push origin "v${release_tag}"
+
+# Create release
+echo -e "\nType (or paste) release notes, press ctrl+d when done\n"
+gh release create "v${release_tag}" --title "v${release_tag}" --verify-tag -F -
+
+# Download and sign auto-generated source tarball and checksum
+gh release download "v${release_tag}" --archive tar.gz --clobber
+gpg --local-user D33FAA16B937F3B2 --armor --detach-sign "zaman-${release_tag}.tar.gz"
+sha256sum "zaman-${release_tag}.tar.gz" > "zaman-${release_tag}.tar.gz.sha256"
+gpg --local-user D33FAA16B937F3B2 --armor --detach-sign "zaman-${release_tag}.tar.gz.sha256"
+
+# Upload source tarball and checksum signatures
+gh release upload "v${release_tag}" "zaman-${release_tag}.tar.gz.asc" "zaman-${release_tag}.tar.gz.sha256" "zaman-${release_tag}.tar.gz.sha256.asc"
+rm -f "zaman-${release_tag}.tar.gz"*
